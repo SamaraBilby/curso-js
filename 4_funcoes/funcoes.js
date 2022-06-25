5a93e5183459cda2cd3ec8289a9df80fb46aba62
SamaraBilby <samarabilby@hotmail.com>
2022-06-25 13:17:18 -0400

    escopo e funções aninhadas

--- 4_funcoes/funcoes.js
+++ 4_funcoes/funcoes.js
@@ -16,4 +16,37 @@ let saudacao = function(nome){
         return `Olá ${nome}`
     }
 }
-console.log(saudacao("Samara"))
+console.log(saudacao("Samara"));
+
+// escopo de função
+let n = 10; 
+    const numero = function() { 
+            let n = 25;
+            console.log(n);
+        }
+    numero();
+    console.log(n);
+
+    let x = 10;
+    if(true)
+    {
+            let x = 20;
+            console.log(x); //escopo if
+    }
+    console.log(x) // escopo geral
+
+// escopo aninhado (Nested Scopes)
+
+let y = 5;
+
+const multiplicar = function(n){
+    let y = n * 2;
+    console.log(y) // escopo da função
+
+    if (y == 10) {
+        let y = 55;
+        console.log(y) // escopo if dentro da função
+    }
+}
+
+multiplicar(5)
