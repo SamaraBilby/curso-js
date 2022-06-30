666ecc5bc6095d4d6da6fbae3f9a62925e33dea1
SamaraBilby <samarabilby@hotmail.com>
2022-06-30 11:44:04 -0400

    array

---
+++ 5_array/array.js
@@ -0,0 +1,23 @@
+let numeros = [1, 3, 5, 8, 12];
+ console.log(numeros);
+let informacoes = ["Samara", 12, true, "Teste", 2];
+console.log(informacoes);
+
+console.log(numeros[4]);
+console.log(informacoes[0]);
+console.log(informacoes[informacoes.length - 1]);
+
+let marca = "Nike";
+
+        console.log(typeof marca.toUpperCase);
+        console.log(marca.toUpperCase());
+
+
+let pessoa = {
+            nome: "Samara",
+            profissao: "Desenvolvedora Front End",
+            idade: 33,
+        }
+
+console.log(pessoa.nome);
+console.log(idade);
