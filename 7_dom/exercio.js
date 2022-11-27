06189859b34c8d1ee8af5a6c843c2b5411416a92
SamaraBilby <samarabilby@hotmail.com>
2022-11-27 18:20:05 -0300

    exerc 40 extends

--- 7_dom/exercio.js
+++ 7_dom/exercio.js
@@ -165,7 +165,7 @@ class Carro{
      }
 }
 
-let carro = new Carro("Renout","Kwid",100, 14)
+let carro = new Carro("Renoult","Kwid",100, 14)
 
     console.log(carro)
 
@@ -176,3 +176,74 @@ let carro = new Carro("Renout","Kwid",100, 14)
     carro.abastecer(100);
 
     console.log(carro);
+
+    // Exercício 40
+
+    class Conta{
+      constructor(saldoCC, saldoCP, juros){
+          this.saldoCC = saldoCC;
+          this.saldoCP = saldoCP;
+          this.juros = juros;
+      }
+
+      deposito(valor){
+        this.saldoCC += valor;
+
+      }
+
+      saque(valor){
+        this.saldoCC -= valor;
+      }
+
+      transferenciaCP(valor){
+        this.saldoCC -= valor;
+        this.saldoCP += valor
+      }
+
+      transferenciaCC(valor){
+        this.saldoCP -= valor;
+        this.saldoCC += valor
+      }
+
+      jurosAniversario() {
+        let juros = (this.saldoCP * this.juros) / 100
+        this.saldoCP += juros;
+      }
+
+    }
+
+    class ContaEspecial extends Conta{
+      constructor(saldoCC, saldoCP, juros){
+        super(saldoCC, saldoCP, juros*2)
+      }      
+    }
+
+    let conta = new Conta(1000, 500, 1);
+    console.log(conta);
+
+    conta.saque(50)
+
+    console.log(conta)
+
+    conta.deposito(500)
+
+    console.log(conta)
+
+    conta.transferenciaCP(1000)
+
+    console.log(conta)
+
+    conta.transferenciaCC(500)
+
+    console.log(conta)
+
+    conta.jurosAniversario()
+
+    console.log(conta)
+
+    let contaEspecial = new ContaEspecial(10000, 50000, 1);
+
+    console.log(contaEspecial);
+
+    contaEspecial.jurosAniversario();
+    console.log(contaEspecial)
