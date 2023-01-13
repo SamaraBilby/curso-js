60e9a057cfb3cbd8ad62ca59c1780b9a37bb2d23
SamaraBilby <samarabilby@hotmail.com>
2023-01-13 11:19:09 -0300

    encontrando elemento c/ querySelector

--- 11_dev_nav/navegador.js
+++ 11_dev_nav/navegador.js
@@ -6,11 +6,19 @@ console.log(document.body.childNodes); // acessa os nós
 console.log(document.body.childNodes[1]); // acessa o nó específico
 console.log(document.body.childNodes[7]);
 
-// encontrando elementos por TAG - documente.getElementByTagName("h2")
+// encontrando elementos por TAG - documente.getElementsByTagName("h2")
 
 console.log(document.getElementsByTagName("h2"));
 console.log(document.getElementsByTagName("ul"));
 console.log(document.getElementsByTagName("li"));
 
+// encontrando elementos por ID - documente.getElementByID("algum_id")
 console.log(document.getElementById("algum_id"));
+// encontrando elementos por ID - documente.getElementsByClassName("alguma_class")
 console.log(document.getElementsByClassName("lista"));
+
+
+// Encontrando elementos com querySelector
+
+console.log(document.querySelector("ul.lista li"));
+console.log(document.querySelector("#algum_id"));
